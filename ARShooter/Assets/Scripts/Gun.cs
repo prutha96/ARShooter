548300a7caedcfd265cae6005e9cbdb4db86a137
prutha96 <pruthadubhashi@gmail.com>
2017-01-26 19:43:39 +0530

    First Commit

---
+++ ARShooter/Assets/Scripts/Gun.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform spawnPoint;
+	public float bulletSpeed;
+
+	void FixedUpdate () {
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			GameObject go = Instantiate (bulletPrefab, spawnPoint.position, spawnPoint.rotation) as GameObject;
+			go.GetComponent<Rigidbody> ().AddForce (transform.forward * bulletSpeed, ForceMode.VelocityChange);
+		}
+	}
+}
